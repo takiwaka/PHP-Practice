db559a2bd799d4276a4776f75611538d7803e9aa
Kazuya Kawakita <(hakusou.kokuyou@gmail.com)>
2022-01-30 08:48:04 +0900

    繰り返し

--- lesson01/sample06.php
+++ lesson01/sample06.php
@@ -1,10 +1,14 @@
 <?php
-$i = 1;
-echo $i . '<br>';
+      $i = 1;
+      while ($i < 366) :
+        echo $i . '日<br>';
+        $i = $i + 1;
+      endwhile; ?>
 
-while ($i < 365) :
-  $i = $i + 1;
-  echo $i . '<br>';
-endwhile; ?>
+<?php
+for ($day = 1; $day < 366; $day++) :
+  echo $day . '日<br />';
+endfor;
+?>
 
 <br />
